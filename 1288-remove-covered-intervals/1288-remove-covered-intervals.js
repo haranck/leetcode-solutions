bd4ea0eb6a307468b0666f32bcc8315821b3dc93
Haran Krishna Ck <harankrishna03@gmail.com>
2026-07-06 12:33:57 +0530

    Time: 2 ms (100%), Space: 55.6 MB (100%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.js
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.js
@@ -4,14 +4,10 @@
  */
 var removeCoveredIntervals = function (intervals) {
     intervals.sort((a, b) => a[0] - b[0])
-
     for (let i = 0; i < intervals.length; i++) {
-
         for (let j = 0; j < intervals.length; j++) {
             if (i === j) continue
-
             if (intervals[i][0] <= intervals[j][0] && intervals[i][1] >= intervals[j][1]) {
-
                 intervals.splice(j, 1)
                 if (j < i) {
                     i--
@@ -20,6 +16,5 @@ var removeCoveredIntervals = function (intervals) {
             }
         }
     }
-    // console.log(intervals.length)
     return intervals.length
 };
