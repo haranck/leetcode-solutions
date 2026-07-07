9019ee2f53b203362ac8be444fd42f72867b8233
Haran Krishna Ck <harankrishna03@gmail.com>
2026-07-07 10:48:18 +0530

    Time: 1 ms (89.29%), Space: 56.1 MB (39.29%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.js
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.js
@@ -3,14 +3,10 @@
  * @return {number}
  */
 var sumAndMultiply = function (n) {
-    let str = n.toString().split('')
+    let str = n.toString().split('0').join('').split('')
     let x = 0
-    let s = ''
     for (let num of str) {
-        if (Number(num) !==0 ){
-            x+=Number(num)
-            s+=num
-        }
+            x+= +num
     }
-    return s*x
+    return str.join('') * x
 };
