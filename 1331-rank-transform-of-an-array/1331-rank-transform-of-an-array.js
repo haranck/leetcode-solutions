aaa68fe750b31899ce4fee3a7ded833fcbca9110
Haran Krishna Ck <harankrishna03@gmail.com>
2026-07-12 16:31:10 +0530

    Time: 108 ms (12.78%), Space: 82.3 MB (24.81%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.js
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.js
@@ -3,20 +3,16 @@
  * @return {number[]}
  */
 var arrayRankTransform = function(arr) {
+    
     let newarr = []
     let uniq  = [...arr].sort((a,b)=>a-b)
-    console.log('uniq ',uniq)
     let sort = [...new Set(uniq)]
-    console.log('sort ',sort)
     let obj  = {}
     for(let i =0;i<sort.length;i++){
         obj[sort[i]] = i + 1;
     }
-    console.log(obj)
     for(let i = 0 ;i<arr.length;i++){
         newarr.push(obj[arr[i]])
     }
-    console.log(newarr)
     return newarr
-    
 };
