008d0c81d6b2611166c73f1e81a58027fe5dd314
Haran Krishna Ck <harankrishna03@gmail.com>
2026-07-25 14:26:28 +0530

    Time: 1 ms (80.85%), Space: 57.1 MB (36.17%) - LeetHub

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var maxProduct = function (n) {
+    let num = String(n).split('')
+    let max = 0
+    for (let i = 0; i < num.length; i++) {
+        for(let j = i+1;j<num.length;j++){
+            let product = Number(num[i]) * Number(num[j])
+            max = Math.max(product,max)
+        }
+    }
+    return max
+};
