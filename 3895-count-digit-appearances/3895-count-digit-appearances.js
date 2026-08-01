87024580afb71e21312fc2dcef1d54aca574f255
Haran Krishna Ck <harankrishna03@gmail.com>
2026-08-01 09:53:46 +0530

    Time: 38 ms (5.48%), Space: 61 MB (19.18%) - LeetHub

---
+++ 3895-count-digit-appearances/3895-count-digit-appearances.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} digit
+ * @return {number}
+ */
+var countDigitOccurrences = function(nums, digit) {
+    let num = ""
+    let count= 0
+    for(let n of nums){
+        num+=n
+    }
+    for(let char of num){
+        if(Number(char)===digit){
+            count++
+        }
+    }
+    console.log(count)
+    console.log(num)
+    return count
+};
