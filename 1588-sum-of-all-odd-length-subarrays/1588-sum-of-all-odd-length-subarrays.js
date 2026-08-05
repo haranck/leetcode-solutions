851372ff4e6ac5ad42a3037c5d8412b746d45953
Haran Krishna Ck <harankrishna03@gmail.com>
2026-08-05 09:34:46 +0530

    Time: 9 ms (6.35%), Space: 60.3 MB (5.55%) - LeetHub

---
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var sumOddLengthSubarrays = function (arr) {
+    let sum = 0
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i; j < arr.length; j++) {
+            let splits = arr.slice(i, j+1)
+            if (splits.length % 2 !== 0) {
+                sum += splits.reduce((acc, curr) => acc + curr, 0)
+            }
+        }
+    }
+    console.log(sum)
+    return sum
+};
