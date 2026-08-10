a56f9b6aa412524c31dcbb6b0dc207297776a6df
Haran Krishna Ck <harankrishna03@gmail.com>
2026-08-10 09:24:32 +0530

    Time: 24 ms (83.33%), Space: 56.2 MB (85.42%) - LeetHub

--- 2843-count-symmetric-integers/2843-count-symmetric-integers.js
+++ 2843-count-symmetric-integers/2843-count-symmetric-integers.js
@@ -7,7 +7,7 @@ var countSymmetricIntegers = function (low, high) {
     let count = 0
     for (let i = low; i <= high; i++) {
         const num = String(i)
-        // if (num.length % 2 !== 0) continue
+        if (num.length % 2 !== 0) continue
         let mid = num.length / 2
         let left = 0
         let right = 0
