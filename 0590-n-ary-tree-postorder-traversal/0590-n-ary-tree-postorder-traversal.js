556aaca8e0d5f746a00cc418f09c6eaad4a3220b
Haran Krishna Ck <harankrishna03@gmail.com>
2026-08-12 17:26:49 +0530

    Time: 67 ms (38.22%), Space: 60.3 MB (65.61%) - LeetHub

---
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.js
@@ -0,0 +1,31 @@
+/**
+ * // Definition for a _Node.
+ * function _Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {_Node|null} root
+ * @return {number[]}
+ */
+var postorder = function(root) {
+    let result = [];
+
+    function traverse(node) {
+        if (node === null) {
+            return;
+        }
+
+        for (let child of node.children) {
+            traverse(child);
+        }
+
+        result.push(node.val);
+    }
+
+    traverse(root);
+
+    return result;
+};
