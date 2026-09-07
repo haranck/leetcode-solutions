03bc4e812d1d16a8db330df16a0ebebde6005610
Haran Krishna Ck <harankrishna03@gmail.com>
2026-09-07 10:31:42 +0530

    Time: 4 ms (20.64%), Space: 59.2 MB (13.24%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.js
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.js
@@ -3,21 +3,19 @@
  * @param {number} k
  * @return {number}
  */
-
-var firstStableIndex = function(nums, k) {
-
+var firstStableIndex = function (nums, k) {
+    let arr = []
+    let index = 0
     for (let i = 1; i <= nums.length; i++) {
-
-        let first = nums.slice(0, i);
-        let second = nums.slice(i - 1, nums.length);
-
-        let max = Math.max(...first);
-        let min = Math.min(...second);
-
-        if (max - min <= k) {
-            return i - 1;
+        let first = nums.slice(0, i)
+        let second = nums.slice(i - 1, nums.length)
+        let max = Math.max(...first)
+        let min = Math.min(...second)
+        arr.push((max - min))
+        if ((max - min) <= k) {
+            index = (max - min)
+            break
         }
     }
-
-    return -1;
+    return arr.indexOf(index)
 };
