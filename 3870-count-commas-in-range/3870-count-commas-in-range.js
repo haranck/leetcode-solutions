7829a5968452e5ac3b8894c2c5e6ab3c735ce67b
Haran Krishna Ck <harankrishna03@gmail.com>
2026-09-08 10:17:16 +0530

    Time: 23 ms (9.68%), Space: 55.8 MB (32.26%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.js
+++ 3870-count-commas-in-range/3870-count-commas-in-range.js
@@ -5,12 +5,10 @@
 var countCommas = function (n) {
     let count = 0
     for (let i = 1; i <= n; i++) {
-        if (i < 1000) {
+        if (i >= 1000) {
             // return 0
-            // count++
-        } else if (n >= 1000) {
             count++
-        }
+        } 
     }
     console.log(count)
     return count
